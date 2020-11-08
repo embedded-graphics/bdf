cb62f8aecad5a5bce782cb406e8b736057f06579
Ralf Fuest <mail@rfuest.de>
2020-11-08 20:14:14 +0100

    Replace BDFParser by FromStr impl for BdfFont

--- bdf-parser/src/lib.rs
+++ bdf-parser/src/lib.rs
@@ -1,6 +1,14 @@
+use std::str::FromStr;
+
 use nom::{
-    bytes::complete::tag, character::complete::multispace0, character::complete::space1,
-    combinator::map, combinator::opt, multi::many0, sequence::separated_pair, IResult,
+    bytes::complete::tag,
+    character::complete::multispace0,
+    character::complete::space1,
+    combinator::map,
+    combinator::{eof, opt},
+    multi::many0,
+    sequence::{separated_pair, terminated},
+    IResult,
 };
 
 mod glyph;
@@ -14,46 +22,45 @@ use metadata::*;
 use properties::*;
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct BDFFont {
+pub struct BdfFont {
     metadata: Option<Metadata>,
     glyphs: Vec<Glyph>,
     properties: Option<Properties>,
 }
 
-pub struct BDFParser<'a> {
-    source: &'a str,
+impl BdfFont {
+    fn parse(input: &[u8]) -> IResult<&[u8], Self> {
+        let (input, metadata) = opt(header)(input)?;
+        let (input, _) = multispace0(input)?;
+        let (input, properties) = opt(properties)(input)?;
+        let (input, _) = multispace0(input)?;
+        let (input, _) = opt(numchars)(input)?;
+        let (input, _) = multispace0(input)?;
+        let (input, glyphs) = many0(glyph)(input)?;
+        let (input, _) = multispace0(input)?;
+        let (input, _) = opt(tag("ENDFONT"))(input)?;
+        let (input, _) = multispace0(input)?;
+
+        Ok((
+            input,
+            Self {
+                properties,
+                metadata,
+                glyphs,
+            },
+        ))
+    }
 }
 
-impl<'a> BDFParser<'a> {
-    pub fn from_str(source: &'a str) -> Self {
-        Self { source }
+impl FromStr for BdfFont {
+    // TODO: use better error type
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let (_, font) = terminated(Self::parse, eof)(&input.as_bytes()).map_err(|_| ())?;
+
+        Ok(font)
     }
-
-    pub fn parse(&self) -> IResult<&[u8], BDFFont> {
-        bdf(&self.source.as_bytes())
-    }
-}
-
-fn bdf(input: &[u8]) -> IResult<&[u8], BDFFont> {
-    let (input, metadata) = opt(header)(input)?;
-    let (input, _) = multispace0(input)?;
-    let (input, properties) = opt(properties)(input)?;
-    let (input, _) = multispace0(input)?;
-    let (input, _) = opt(numchars)(input)?;
-    let (input, _) = multispace0(input)?;
-    let (input, glyphs) = many0(glyph)(input)?;
-    let (input, _) = multispace0(input)?;
-    let (input, _) = opt(tag("ENDFONT"))(input)?;
-    let (input, _) = multispace0(input)?;
-
-    Ok((
-        input,
-        BDFFont {
-            properties,
-            metadata,
-            glyphs,
-        },
-    ))
 }
 
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
@@ -75,8 +82,6 @@ impl BoundingBox {
 mod tests {
     use super::*;
     use maplit::hashmap;
-
-    const EMPTY: &[u8] = &[];
 
     #[test]
     fn it_parses_a_font_file() {
@@ -108,49 +113,46 @@ ENDCHAR
 ENDFONT
 "#;
 
-        let out = bdf(&chardata.as_bytes());
+        let font = BdfFont::from_str(chardata).unwrap();
 
         assert_eq!(
-            out,
-            Ok((
-                EMPTY,
-                BDFFont {
-                    metadata: Some(Metadata {
-                        version: 2.1,
-                        name: String::from("\"test font\""),
-                        size: (16, (75, 75)),
+            font,
+            BdfFont {
+                metadata: Some(Metadata {
+                    version: 2.1,
+                    name: String::from("\"test font\""),
+                    size: (16, (75, 75)),
+                    bounding_box: BoundingBox {
+                        size: (16, 24),
+                        offset: (0, 0),
+                    }
+                }),
+                glyphs: vec![
+                    Glyph {
+                        bitmap: vec![0x1f01],
                         bounding_box: BoundingBox {
-                            size: (16, 24),
+                            size: (8, 8),
                             offset: (0, 0),
-                        }
-                    }),
-                    glyphs: vec![
-                        Glyph {
-                            bitmap: vec![0x1f01],
-                            bounding_box: BoundingBox {
-                                size: (8, 8),
-                                offset: (0, 0),
-                            },
-                            charcode: 64,
-                            name: "000".to_string(),
                         },
-                        Glyph {
-                            bitmap: vec![0x2f02],
-                            bounding_box: BoundingBox {
-                                size: (8, 8),
-                                offset: (0, 0),
-                            },
-                            charcode: 64,
-                            name: "000".to_string(),
+                        charcode: 64,
+                        name: "000".to_string(),
+                    },
+                    Glyph {
+                        bitmap: vec![0x2f02],
+                        bounding_box: BoundingBox {
+                            size: (8, 8),
+                            offset: (0, 0),
                         },
-                    ],
-                    properties: Some(hashmap! {
-                        "COPYRIGHT".into() => PropertyValue::Text("https://github.com/iconic/open-iconic, SIL OPEN FONT LICENSE".into()),
-                        "FONT_ASCENT".into() => PropertyValue::Int(0),
-                        "FONT_DESCENT".into() => PropertyValue::Int(0),
-                    })
-                }
-            ))
+                        charcode: 64,
+                        name: "000".to_string(),
+                    },
+                ],
+                properties: Some(hashmap! {
+                    "COPYRIGHT".into() => PropertyValue::Text("https://github.com/iconic/open-iconic, SIL OPEN FONT LICENSE".into()),
+                    "FONT_ASCENT".into() => PropertyValue::Int(0),
+                    "FONT_DESCENT".into() => PropertyValue::Int(0),
+                })
+            }
         );
     }
 
@@ -183,83 +185,78 @@ BITMAP
 ENDCHAR
 "#;
 
-        let out = bdf(&chardata.as_bytes());
+        let font = BdfFont::from_str(chardata).unwrap();
 
         assert_eq!(
-            out,
-            Ok((
-                EMPTY,
-                BDFFont {
-                    metadata: Some(Metadata {
-                        version: 2.1,
-                        name: String::from("\"open_iconic_all_1x\""),
-                        size: (16, (75, 75)),
+            font,
+            BdfFont {
+                metadata: Some(Metadata {
+                    version: 2.1,
+                    name: String::from("\"open_iconic_all_1x\""),
+                    size: (16, (75, 75)),
+                    bounding_box: BoundingBox {
+                        size: (16, 16),
+                        offset: (0, 0),
+                    }
+                }),
+                glyphs: vec![
+                    Glyph {
+                        bitmap: vec![0x1f01],
                         bounding_box: BoundingBox {
-                            size: (16, 16),
-                            offset: (0, 0),
-                        }
-                    }),
-                    glyphs: vec![
-                        Glyph {
-                            bitmap: vec![0x1f01],
-                            bounding_box: BoundingBox {
-                                size: (8, 8),
-                                offset: (0, 0)
-                            },
-                            charcode: 64,
-                            name: "000".to_string(),
+                            size: (8, 8),
+                            offset: (0, 0)
                         },
-                        Glyph {
-                            bitmap: vec![0x2f02],
-                            bounding_box: BoundingBox {
-                                size: (8, 8),
-                                offset: (0, 0)
-                            },
-                            charcode: 64,
-                            name: "000".to_string(),
+                        charcode: 64,
+                        name: "000".to_string(),
+                    },
+                    Glyph {
+                        bitmap: vec![0x2f02],
+                        bounding_box: BoundingBox {
+                            size: (8, 8),
+                            offset: (0, 0)
                         },
-                    ],
-                    properties: Some(hashmap! {
-                        "COPYRIGHT".into() => PropertyValue::Text("https://github.com/iconic/open-iconic, SIL OPEN FONT LICENSE".into()),
-                        "FONT_ASCENT".into() => PropertyValue::Int(0),
-                        "FONT_DESCENT".into() => PropertyValue::Int(0),
-                    })
-                }
-            ))
+                        charcode: 64,
+                        name: "000".to_string(),
+                    },
+                ],
+                properties: Some(hashmap! {
+                    "COPYRIGHT".into() => PropertyValue::Text("https://github.com/iconic/open-iconic, SIL OPEN FONT LICENSE".into()),
+                    "FONT_ASCENT".into() => PropertyValue::Int(0),
+                    "FONT_DESCENT".into() => PropertyValue::Int(0),
+                })
+            }
         );
     }
 
     #[test]
     fn it_handles_windows_line_endings() {
         let windows_line_endings = "STARTFONT 2.1\r\nFONT \"windows_test\"\r\nSIZE 10 96 96\r\nFONTBOUNDINGBOX 8 16 0 -4\r\nCHARS 256\r\nSTARTCHAR 0\r\nENCODING 0\r\nSWIDTH 600 0\r\nDWIDTH 8 0\r\nBBX 8 16 0 -4\r\nBITMAP\r\nD5\r\nENDCHAR\r\nENDFONT\r\n";
-        let out = bdf(&windows_line_endings.as_bytes());
+
+        let font = BdfFont::from_str(windows_line_endings).unwrap();
 
         assert_eq!(
-            out,
-            Ok((
-                EMPTY,
-                BDFFont {
-                    metadata: Some(Metadata {
-                        version: 2.1,
-                        name: String::from("\"windows_test\""),
-                        size: (10, (96, 96)),
-                        bounding_box: BoundingBox {
-                            size: (8, 16),
-                            offset: (0, -4)
-                        },
-                    }),
-                    glyphs: vec![Glyph {
-                        bitmap: vec![0xd5],
-                        bounding_box: BoundingBox {
-                            size: (8, 16),
-                            offset: (0, -4)
-                        },
-                        charcode: 0,
-                        name: "0".to_string(),
-                    },],
-                    properties: None
-                }
-            ))
+            font,
+            BdfFont {
+                metadata: Some(Metadata {
+                    version: 2.1,
+                    name: String::from("\"windows_test\""),
+                    size: (10, (96, 96)),
+                    bounding_box: BoundingBox {
+                        size: (8, 16),
+                        offset: (0, -4)
+                    },
+                }),
+                glyphs: vec![Glyph {
+                    bitmap: vec![0xd5],
+                    bounding_box: BoundingBox {
+                        size: (8, 16),
+                        offset: (0, -4)
+                    },
+                    charcode: 0,
+                    name: "0".to_string(),
+                },],
+                properties: None
+            }
         );
     }
 }
